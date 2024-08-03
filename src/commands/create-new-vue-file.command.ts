6d277b04319b92280d7598827b6ed821649ef0dd
K-eL <kael81@gmail.com>
2024-08-04 00:30:46 +0200

    feat: check the users settings to use indentation values

--- src/commands/create-new-vue-file.command.ts
+++ src/commands/create-new-vue-file.command.ts
@@ -1,51 +1,75 @@
-
-import * as vscode from 'vscode';
-import { requestStringDialog } from '../helpers/editor-helper';
-import { createFile, handleVueFileName, isFileNameValid, openFile } from '../helpers/file-helper';
-import { VueApiType } from '../enums/vue-api-type.enum';
-import { FileSettings } from '../interfaces/file-settings';
-import { VueStyleLang } from '../enums/vue-style-lang.enum';
-import { VueScriptLang } from '../enums/vue-script-lang.enum';
-import { generateContent } from '../generators/content.generator';
-import { isScriptFirst, loadWorkspaceConfig } from '../helpers/config.helper';
+import * as vscode from "vscode";
+import { requestStringDialog } from "../helpers/editor.helper";
+import {
+	createFile,
+	handleVueFileName,
+	openFile,
+} from "../helpers/file.helper";
+import { VueApiType } from "../enums/vue-api-type.enum";
+import { FileSettings } from "../interfaces/file-settings";
+import { VueStyleLang } from "../enums/vue-style-lang.enum";
+import { VueScriptLang } from "../enums/vue-script-lang.enum";
+import { generateContent } from "../generators/content.generator";
+import { ConfigHelper } from "../helpers/config.helper";
 
 const templateCursorPosition = new vscode.Position(2, 2);
 const scriptCursorPosition = new vscode.Position(2, 0);
 
-export const createNewVueFileCommand = async (uri: any, apiType: VueApiType, scriptLang: VueScriptLang, styleLang: VueStyleLang) => {
+/**
+ * Creates a new Vue file. It asks for the component name and creates the file. The cursor position is then determined and the file is finally opened in the editor.
+ * @param uri The uri of the file. It can be either a file or a folder
+ * @param apiType The type of the api. It can be either setup or options
+ * @param scriptLang The language of the script. It can be either TypeScript or JavaScript
+ * @param styleLang The language of the style. It can be either CSS, SCSS
+ * @returns
+ */
+
+export const createNewVueFileCommand = async (
+	uri: vscode.Uri,
+	apiType: VueApiType,
+	scriptLang: VueScriptLang,
+	styleLang: VueStyleLang,
+) => {
 	// loads/updates workspace config
-	loadWorkspaceConfig();
+	const configHelper = new ConfigHelper();
 
 	// get file name from user input
-	let stringInput = await requestStringDialog(
-		'Enter the name of the new component file',
-		'MyComponent',
-		isFileNameValid
+	const stringInput = await requestStringDialog(
+		"Enter the name of the new component file",
+		"MyComponent",
 	);
 
 	// return if no file name was entered
-	if (!stringInput) { return; }
+	if (!stringInput) {
+		return;
+	}
 
 	// handle file name
 	const fileName = handleVueFileName(stringInput);
 	// handle component name
-	const componentName = fileName.replace('.vue', '').split('-').map((word) => {
-		return word.charAt(0).toUpperCase() + word.slice(1);
-	}).join('');
+	const componentName = fileName
+		.replace(".vue", "")
+		.split("-")
+		.map(word => {
+			return word.charAt(0).toUpperCase() + word.slice(1);
+		})
+		.join("");
 
 	const newFileSettings: FileSettings = {
 		isSetupApi: apiType === VueApiType.setup,
 		scriptLang,
 		styleLang,
-		componentName
+		componentName,
 	};
 
 	// define the file content
-	const fileContent = generateContent(newFileSettings);
+	const fileContent = generateContent(newFileSettings, configHelper);
 
 	// if uri contains a file, use the parent folder
-	if (uri.fsPath.includes('.')) {
-		uri = uri.with({ path: uri.path.replace(uri.fsPath.split('/').pop() as string, '') });
+	if (uri.fsPath.includes(".")) {
+		uri = uri.with({
+			path: uri.path.replace(uri.fsPath.split("/").pop() as string, ""),
+		});
 	}
 	// define the new file uri
 	const newFileUri = uri.with({ path: `${uri.path}/${fileName}` });
@@ -54,6 +78,8 @@ export const createNewVueFileCommand = async (uri: any, apiType: VueApiType, scr
 	await createFile(newFileUri, fileContent);
 
 	// open the new file
-	const cursorPosition = isScriptFirst() ? scriptCursorPosition : templateCursorPosition;
+	const cursorPosition = configHelper.isScriptFirst()
+		? scriptCursorPosition
+		: templateCursorPosition;
 	await openFile(newFileUri, cursorPosition);
 };
--- src/helpers/config.helper.ts
+++ src/helpers/config.helper.ts
@@ -1,157 +1,272 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-let config: vscode.WorkspaceConfiguration | undefined;
+export class ConfigHelper {
+	private _editorConfig: vscode.WorkspaceConfiguration | undefined;
+	private _vueFilesConfig: vscode.WorkspaceConfiguration | undefined;
+	private _useTabs = false;
+	private _tabSize = 2;
 
-export const loadWorkspaceConfig = (): vscode.WorkspaceConfiguration => {
-	config = vscode.workspace.getConfiguration('vscode-vue-files');
-	return config;
-};
+	constructor() {
+		this.loadVueFilesConfig();
+		this.loadEditorConfig();
+		this.loadIndentConfig();
+	}
 
-export const isScriptFirst = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('fileStructure.scriptTagComesFirst') as boolean ?? false;
-};
+	private loadVueFilesConfig(): void {
+		try {
+			this._vueFilesConfig ??=
+				vscode.workspace.getConfiguration("vscode-vue-files");
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-export const showVModelTemplate = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('template.showV-ModelTemplate') as boolean ?? true;
-};
+	private loadEditorConfig(): void {
+		try {
+			this._editorConfig ??= vscode.workspace.getConfiguration("editor");
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
-export const showNameScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showNameScriptOption') as boolean ?? true;
-};
+	private loadIndentConfig(): void {
+		this._useTabs ??= !this._editorConfig?.get("insertSpaces");
+		this._tabSize ??= this._editorConfig?.get("tabSize") || 2;
+	}
 
-export const showComponentsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showComponentsScriptOption') as boolean ?? true;
-};
+	public ind = (x: number = 1): string => {
+		return this._useTabs ? "\t".repeat(x) : " ".repeat(this._tabSize * x);
+	};
 
-export const showDirectivesScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showDirectivesScriptOption') as boolean ?? true;
-};
+	public isScriptFirst(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"fileStructure.scriptTagComesFirst",
+			) as boolean) ?? false
+		);
+	}
 
-export const showExtendsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showExtendsScriptOption') as boolean ?? true;
-};
+	public showVModelTemplate(): boolean {
+		return (
+			(this._vueFilesConfig?.get("template.showV-ModelTemplate") as boolean) ??
+			true
+		);
+	}
 
-export const showMixinsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showMixinsScriptOption') as boolean ?? true;
-};
+	public showNameScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showNameScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showProvideInjectScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showProvideInjectScriptOption') as boolean ?? true;
-};
+	public showComponentsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showComponentsScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showInheritAttrsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showInheritAttributesScriptOption') as boolean ?? true;
-};
+	public showDirectivesScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showDirectivesScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showPropsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showPropsScriptOption') as boolean ?? true;
-};
+	public showExtendsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showExtendsScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showEmitsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showEmitsScriptOption') as boolean ?? true;
-};
+	public showMixinsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showMixinsScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showSetupScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showSetupScriptOption') as boolean ?? true;
-};
+	public showProvideInjectScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showProvideInjectScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showDataScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showDataScriptOption') as boolean ?? true;
-};
+	public showInheritAttrsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showInheritAttributesScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showComputedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showComputedScriptOption') as boolean ?? true;
-};
-export const showWatchScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showWatchScriptOption') as boolean ?? true;
-};
+	public showPropsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showPropsScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showLifecycleHooksScriptOptions = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showLifecycleHooksScriptOptions') as boolean ?? true;
-};
+	public showEmitsScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showEmitsScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showBeforeCreateScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showBeforeCreateScriptOption') as boolean ?? true;
-};
+	public showSetupScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showSetupScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showCreatedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showCreatedScriptOption') as boolean ?? true;
-};
+	public showDataScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showDataScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showBeforeMountScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showBeforeMountScriptOption') as boolean ?? true;
-};
+	public showComputedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showComputedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showMountedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showMountedScriptOption') as boolean ?? true;
-};
+	public showWatchScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get("option.showWatchScriptOption") as boolean) ??
+			true
+		);
+	}
 
-export const showBeforeUpdateScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showBeforeUpdateScriptOption') as boolean ?? true;
-};
+	public showLifecycleHooksScriptOptions(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showLifecycleHooksScriptOptions",
+			) as boolean) ?? true
+		);
+	}
 
-export const showUpdatedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showUpdatedScriptOption') as boolean ?? true;
-};
+	public showBeforeCreateScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showBeforeCreateScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showActivatedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showActivatedScriptOption') as boolean ?? true;
-};
+	public showCreatedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showCreatedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showDeactivatedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showDeactivatedScriptOption') as boolean ?? true;
-};
+	public showBeforeMountScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showBeforeMountScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showBeforeUnmountScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showBeforeUnmountScriptOption') as boolean ?? true;
-};
+	public showMountedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showMountedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showUnmountedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showUnmountedScriptOption') as boolean ?? true;
-};
+	public showBeforeUpdateScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showBeforeUpdateScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showErrorCapturedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showErrorCapturedScriptOption') as boolean ?? true;
-};
+	public showUpdatedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showUpdatedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showRenderTrackedScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showRenderTrackedScriptOption') as boolean ?? true;
-};
+	public showActivatedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showActivatedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showRenderTriggeredScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('lifecycle.showRenderTriggeredScriptOption') as boolean ?? true;
-};
+	public showDeactivatedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showDeactivatedScriptOption",
+			) as boolean) ?? true
+		);
+	}
 
-export const showMethodsScriptOption = (): boolean => {
-	config ??= loadWorkspaceConfig();
-	return config?.get('option.showMethodsScriptOption') as boolean ?? true;
-};
+	public showBeforeUnmountScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showBeforeUnmountScriptOption",
+			) as boolean) ?? true
+		);
+	}
+
+	public showUnmountedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showUnmountedScriptOption",
+			) as boolean) ?? true
+		);
+	}
+
+	public showErrorCapturedScriptOption(): boolean {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showErrorCapturedScriptOption",
+			) as boolean) ?? true
+		);
+	}
+
+	public showRenderTrackedScriptOption = (): boolean => {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showRenderTrackedScriptOption",
+			) as boolean) ?? true
+		);
+	};
+
+	public showRenderTriggeredScriptOption = (): boolean => {
+		return (
+			(this._vueFilesConfig?.get(
+				"lifecycle.showRenderTriggeredScriptOption",
+			) as boolean) ?? true
+		);
+	};
+
+	public showMethodsScriptOption = (): boolean => {
+		return (
+			(this._vueFilesConfig?.get(
+				"option.showMethodsScriptOption",
+			) as boolean) ?? true
+		);
+	};
+}
